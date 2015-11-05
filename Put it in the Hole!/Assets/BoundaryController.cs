07278b9b59b85de55c32c8f187ebea18ef50b6a8
Kyle Reddeman <kyler7662@gmail.com>
2015-11-04 21:00:50 -0800

    Added basic functions

---
+++ Put it in the Hole!/Assets/BoundaryController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundaryController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Time.timeScale = 0;
+    }
+}
---
+++ Put it in the Hole!/Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    public Rigidbody2D rb;
+
+    float x = 0;
+    float y = 0;
+    bool hasShot = false;
+    Vector2 direction = new Vector2();
+
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        // transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        if (hasShot == false)
+        { 
+            transform.RotateAround(Vector2.zero, Vector3.forward, speed * Time.deltaTime);
+            x = transform.position.x;
+            y = transform.position.y;
+        }
+        else
+        {
+                transform.Translate(new Vector3(-y, x) * 3 * Time.deltaTime , Space.World);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hasShot = true;
+        }
+        
+        
+
+    }
+    
+}
