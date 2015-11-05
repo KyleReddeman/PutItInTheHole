606388c3190d9be0453f42e7de33fab488f34500
Kyle Reddeman <kyler7662@gmail.com>
2015-11-05 12:11:43 -0800

    Changed how player orbits

--- Put it in the Hole!/Assets/PlayerController.cs
+++ Put it in the Hole!/Assets/PlayerController.cs
@@ -7,23 +7,26 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Rigidbody2D rb;
 
+    GameObject sun; 
+
     float x = 0;
     float y = 0;
     bool hasShot = false;
     Vector2 direction = new Vector2();
 
 	void Start () {
-	
-	}
+	    sun = GameObject.Find("Sun");
+    }
 
     // Update is called once per frame
     void Update() {
         // transform.Translate(Vector3.up * Time.deltaTime, Space.World);
         if (hasShot == false)
-        { 
-            transform.RotateAround(Vector2.zero, Vector3.forward, speed * Time.deltaTime);
-            x = transform.position.x;
-            y = transform.position.y;
+        {
+            
+            transform.RotateAround(sun.transform.position, Vector3.forward, speed * Time.deltaTime);
+            x = transform.position.x - sun.transform.position.x;
+            y = transform.position.y - sun.transform.position.y;
         }
         else
         {
